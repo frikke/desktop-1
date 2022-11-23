90dea77bb9242387a469bc1bdcd8999a386d6d7c
Claudio Cambra <claudio.cambra@nextcloud.com>
2022-11-23 16:51:45 +0100

    Fix QTextCodec related build issues
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/libsync/logger.cpp
+++ src/libsync/logger.cpp
@@ -179,7 +179,6 @@ void Logger::setLogFile(const QString &name)
     }
 
     _logstream.reset(new QTextStream(&_logFile));
-    _logstream->setCodec(QTextCodec::codecForName("UTF-8"));
 }
 
 void Logger::setLogExpire(int expire)
