60c27a15ee9dda519ca8a9f56b488d1c2b593c8b
alex-z <blackslayer4@gmail.com>
2023-05-18 13:50:13 +0200

    Fix compile errors,
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- src/libsync/clientsideencryption.cpp
+++ src/libsync/clientsideencryption.cpp
@@ -70,11 +70,7 @@ constexpr auto e2e_cert_sharing = "_sharing";
 const char e2e_private[] = "_e2e-private";
 const char e2e_mnemonic[] = "_e2e-mnemonic";
 
-constexpr auto metadataKeyJsonKey = "metadataKey";
-
 constexpr qint64 blockSize = 1024;
-
-constexpr auto metadataKeySize = 16;
 
 QList<QByteArray> oldCipherFormatSplit(const QByteArray &cipher)
 {
